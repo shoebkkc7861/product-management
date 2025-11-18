e98d9cd8d1ab8356bf0877b27715c39a234509d0
shoeb786 <mohd.khan@vernost.com>
2025-11-19 04:14:04 +0530

    add validator in user crud

--- routers/user.js
+++ routers/user.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { register, userLogin, deleteUser, updateUser } from "../controller/user.js";
 import { auth } from "../middleware/auth.js";
+import {validate} from "../middleware/validate.js"
+import {signupSchema,loginSchema} from "../validations/user.js"
 
 const router = express.Router();
 
-router.post("/signup", register);
-router.post("/login", userLogin);
+router.post("/signup",validate(signupSchema), register);
+router.post("/login", validate(loginSchema),userLogin);
 
 router.delete("/delete", auth, deleteUser);
 router.put("/update", auth, updateUser);
