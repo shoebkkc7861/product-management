6719a1ab4432d8d123c1316d0462c4fcfe1c2ec6
shoeb786 <mohd.khan@vernost.com>
2025-11-19 04:04:41 +0530

    user validations

---
+++ validations/user.js
@@ -0,0 +1,19 @@
+import Joi from "joi";
+
+export const signupSchema = Joi.object({
+  first_name: Joi.string().required(),
+  last_name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string().min(10).max(15).required(),
+  password: Joi.string().min(6).required(),
+  gender: Joi.string().valid("male", "female", "other").required(),
+  dob: Joi.date().required(),
+  city: Joi.string().required(),
+  state: Joi.string().required(),
+  pincode: Joi.string().required()
+});
+
+export const loginSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required()
+});
