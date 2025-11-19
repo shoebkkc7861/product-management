822fe6e5fe5b69f5cbf4f316dde4fa983c58d111
shoeb786 <mohd.khan@vernost.com>
2025-11-19 22:41:25 +0530

    validations

---
+++ validations/product.js
@@ -0,0 +1,27 @@
+import Joi from "joi";
+
+export const createProductSchema = Joi.object({
+  product_name: Joi.string().required(),
+  sku: Joi.string().required(),
+  image: Joi.string().allow(null, ""),
+  price: Joi.number().positive().required(),
+  category_id: Joi.number().required(),
+  stock: Joi.number().integer().min(0).default(0),
+  description: Joi.string().allow("", null),
+});
+
+export const updateProductSchema = Joi.object({
+  unique_id: Joi.string().required(),
+  product_name: Joi.string(),
+  sku: Joi.string(),
+  image: Joi.string().allow(null, ""),
+  price: Joi.number().positive(),
+  category_id: Joi.number(),
+  stock: Joi.number().integer().min(0),
+  description: Joi.string().allow("", null),
+  is_active: Joi.number().valid(0, 1),
+});
+
+export const deleteProductSchema = Joi.object({
+  unique_id: Joi.string().required(),
+});
